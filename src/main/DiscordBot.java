0e7a5c4356b5f130bec5c4ca4551c744db266f1a
Vincent Bravo (LAPTOP) <nbravo@g.clemson.edu>
2017-02-14 20:33:13 -0500

    got rid of some annoying nullpointer warning (by suppressing it huehue)

--- src/main/DiscordBot.java
+++ src/main/DiscordBot.java
@@ -1,18 +1,18 @@
 package main;
 
 import main.listeners.MessageListener;
+import org.apache.commons.lang3.ObjectUtils;
 import sx.blah.discord.api.ClientBuilder;
 import sx.blah.discord.api.IDiscordClient;
 import sx.blah.discord.api.events.EventDispatcher;
 import sx.blah.discord.util.DiscordException;
-
-import java.util.List;
 
 public class DiscordBot {
     private static final String TOKEN = "MjgxMTkwNTU3NDM3NTI2MDE4.C4UWvQ.DS1Bs85rP9HDwK8Si0nfxTk1anc";
 
     public static void main(String[] args) {
         IDiscordClient client = createClient(TOKEN, true);
+        //noinspection NullableProblems
         EventDispatcher dispatcher = client.getDispatcher();
         dispatcher.registerListener(new MessageListener());
     }
